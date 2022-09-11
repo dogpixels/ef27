15d443cc1a80ac3c5aa3a557cd8b03b4dc4399ea
draconigen <draconigen@gmail.com>
2022-09-11 18:22:12 +0200

    chore: :speech_balloon: update pps job listing

--- www/pages/jobs.php
+++ www/pages/jobs.php
@@ -64,7 +64,7 @@
 				<p>Join the Team - Eurofurence Pawpet Show.</p>
 				<p>	Do you like fur, art, puppetry or gimmicky stage tech? Then we have great news for you!<br />
 					To achieve even greater goals for the future, the Eurofurence Pawpet Team is looking for enthusiastic people who would like to share their love for the fandom by helping to make one of its biggest events even more awesome.<br />
-					The EF Pawpet Show is with 17 years one of the longest running spectacles of the fandom. Every year the team amazes more than 2000 people with emotional adventures, memorable characters and impressive anthropomorphic puppets. Everything you will see at the show is handcrafted and live played by about 50 dedicated team members and amazing volunteers.<br />
+					The EF Pawpet Show is with 20 years one of the longest running spectacles of the fandom. Every year the team amazes more than 2000 people with emotional adventures, memorable characters and impressive anthropomorphic puppets. Everything you will see at the show is handcrafted and live played by about 50 dedicated team members and amazing volunteers.<br />
 					We offer you to join this very team at Eurofurence to apply your individual talents, obtain hands on experience in theatrical production and get to know the people behind “Skies of Astar” and “Keepers of the Light”.<br />
 				</p>
 				<div>
@@ -81,7 +81,7 @@
 				</div>
 				<p>
 					You want to help?<br />
-					Then <a href="https://help.eurofurence.org/contact/pps" target="_blank">send us a note</a> or contact <a href="https://t.me/BestYeen" target="_blank">”@BestYeen” on Telegram</a>.<br />
+					Then <a href="https://help.eurofurence.org/contact/pps" target="_blank">send us a note</a> or contact <a href="https://t.me/ZefiroDragon" target="_blank">”@ZefiroDragon” on Telegram</a>.<br />
 					For more Information and Questions:<br/>
 					<img src="img/pages/jobs/pps.jpg" alt="" />
 					Become an active supporter and join us today.<br />
@@ -90,7 +90,7 @@
 				<p>The EF Pawpet Show Team</p>
 				<div>
 					<p>
-						You don’t have the time but still would like to help?<br />
+						You don't have the time but still would like to help?<br />
 						Than check out our newly created Patreon page and YouTube channel.<br />
 						We regularly share insightful updates about the whole PPS production and even publish unique footage about the shows to come.
 					</p>
