519fe7726bb8f3e92a66592e64c65bac2a0314fd
draconigen <draconigen@gmail.com>
2022-12-04 18:34:16 +0100

    chore: :speech_balloon: add opening ceremony prod assist job

--- www/pages/jobs.php
+++ www/pages/jobs.php
@@ -315,6 +315,34 @@
 		</li>
 	</ul>
 
+	<ul uk-accordion>
+		<li>
+			<a href="#" class="uk-accordion-title">Opening Show: Show Production Assistant</a>
+			<div class="uk-accordion-content uk-column-1-2@l">
+				<p>Our team specializes in creating an animated short movie for all our attendees to enjoy during the Opening Show. It’s embedded in a mixed media show that also includes live action stage elements, music and much more.</p>
+				<p>We are the ones
+					<ul>
+						<li>writing the movie script and the stage directions for the Opening Show,</li>
+						<li>modeling, rigging and creating characters for the Eurofurence Opening Show,</li>
+						<li>talking with each department that makes an appearance on stage about their needs for the Opening Show</li>
+					</ul>
+				</p>
+				<p>Being part of our team is the perfect way to get immersed in the very heart of action at one of Eurofurence's high-traffic events on site. As our production assistant, you are responsible for the successful creation of the Opening Show during the Eurofurence as well as in the months before. You have to be familiar with traditional tools such as paper lists as well as digital media, including how to utilize them. This includes working with Google Docs, PDF-XChange (or similar), Nextcloud and Trello. It would be nice if you can make use of Scrivener and Highland 2.
+					<ul>
+						<li>You are sufficiently fluent in English to communicate within our team, the other departments, our Guest of Honor, and everyone else needed for the Opening Show. Fluency in German and/or other languages would be a big plus.</li>
+						<li>You will assist in directing and running the general rehearsal. Thereby you will help to make sure that all participants are on stage on time. You will assist the stage team with technical questions and know the show schedule by heart. Together with the team you will develop concepts for organization, execution and planning the show. You will handle various side tasks, such as printing and handing out documents.</li>
+						<li>It would be an advantage if you already have experience in show planning and execution at large events. Experience in the field of theater will also help. If you don't have any experience yet, we will teach you everything you need.</li>
+						<li>You have a sense of how to handle even challenging situations in a calm, de-escalating manner, and keep the show running. In stressful situations, you remain collected, professional and solution-focused. Communication and teamwork is essential to run a show in a sometimes stressful environment.</li>
+						<li>You have to spend a few extra hours during the convention. This will include a security training for the stage, as well as the general rehearsal. On Wednesday you will spend six hours on the Opening Show. Everything else is done off-site and online.</li>
+					</ul>
+				</p>
+				<p>Following your application, we will have a brief call with you to get to know you a little bit. A personal conversation between you and the team leader will help all sides to make sure we are on the same page with any questions you may have.</p>
+				<p>Are you interested in joining the Opening Show and becoming our show production assistant? Please contact <a href="https://t.me/pattarchus" target="_blank">@Pattarchus on Telegram</a> and he will get back to you shortly. Thank you for taking the time to read this. The Eurofurence Opening Show team looks forward to hearing from you!</p>
+			</div>
+		</li>
+	</ul>
+
+
 </section>
 
 <hr />
