6edd1b5bd6eda7b46e02ce3184c143018f8efdd5
draconigen <draconigen@gmail.com>
2023-03-24 12:53:46 +0100

    chore: :speech_balloon: Garra: add stage director job

--- www/pages/jobs.php
+++ www/pages/jobs.php
@@ -321,6 +321,63 @@
 		</li>
 	</ul>
 
+	
+	<ul uk-accordion>
+		<li>
+			<a href="#" class="uk-accordion-title">Stage Director (any gender)</a>
+			<div class="uk-accordion-content uk-column-1-2@l">
+				<div>
+					<p>The Eurofurence stage team operates three live event stages at Europe's largest furry convention. The team of 30 volunteers provides infrastructure and services to events big and small, from solo artists to dance competitions and fully-fledged theater shows.
+
+				<p>We want to deliver high-quality entertainment to our guests. To that end, our volunteers work colloratively with the artists from vision to concept to reality.</p>
+
+				<p>To bolster our leadership ranks, we are looking for an additional Stage Director. </p>
+
+
+				<p><strong>Your Responsibilities</strong></p>
+				<p>Eurofurence Directors (commonly called "red badges") are responsible for leading their team of volunteers. As one of four Stage Directors you are going to work shoulder to shoulder with three seasoned veterans of the Eurofurence trade. You will engage in association-wide communication with other directors across different crafts and trades; your engaging and pro-active communication style makes you the perfect contact person for our external and internal partners and together you work out the details of stage planning. You bear the responsibility of production management gracefully, juggling numbers and spreadsheets like it's your second nature.</p>
+
+				<p>The final scope of your position will depend on your skills and interests at the point of application.</p>
+				
+					<p><strong>Your Profile</strong>
+						<ul>
+							<li>Availablity:</li>
+							<ul>
+								<li>5 hours/month (September — April)</li>
+								<li>20 hours / month (May — August)</li>
+								<li>40 hours on-site during Eurofurence</li>
+								<li>Present on-site from early-early arrival until late departure</li>
+							</ul>
+							<li>Committed for at least three years (including Eurofurence 2026)</li>
+							<li>Willing to work flexible shifts and take on-call duty</li>
+							<li>Ready to travel within Central Europe (at least two Stage Crew Meetings, one Annual General Meeting, three Director's Meetings)</li>
+							<li>Fluent in written and spoken English</li>
+							<li>In-depth knowledge of specific office tools, or willingness to learn (e.g. Google Sheets, DokuWiki)</li>
+							<li>Excellent communicator</li>
+							<li>Responsible and self-organising</li>
+							<li>Highly resilient and good conflict manager</li>
+							<li>Basic technical understanding</li>
+							<li>Interest in cross-departemental work</li>
+						</ul>
+					</p>
+				</div>
+
+				<p>Nice-to-Haves:
+					<ul>
+						<li>Fluent in written and spoken German</li>
+						<li>3D-CAD software skills</li>
+						<li>Experienced/trained in event technology or related professions (IT, AV, ...)</li>
+						<li>Experience in organizing annual large-scale events</li>
+					</ul>
+				</p>
+				
+				<p>Aside from the training on the job, we offer an exciting work place with motivated team members, room for creative expression and explicitly invite you to bring along new ideas!</p>
+
+				<p>Do you want to know more? Does this job offer pique your interest? We would love to hear from you! <a href="https://help.eurofurence.org/contact/stage" target="_blank">Send your application (or further questions) via eMail</a>. You can apply until the <em>31.03.2023</em>. You will hear from us within two weeks after the deadline!</p>
+			</div>
+		</li>
+	</ul>
+
 
 </section>
 
