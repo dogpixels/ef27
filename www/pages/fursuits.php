85a33343604e69cebb18f18c65a563ab33d89d46
draconigen <draconigen@gmail.com>
2023-08-27 17:05:07 +0200

    fix: :speech_balloon: update FSL opening times

--- www/pages/fursuits.php
+++ www/pages/fursuits.php
@@ -4,47 +4,48 @@
 	<p>To help you finding your way around the convention, following you can find some information and useful tips for fursuiters at Eurofurence.</p>
 </section>
 
-<section class="two_columns just">	
+<section class="uk-column-1-2@l">	
 
-	<div class="retain">
+	<div>
 		<h4>Fursuit Badges</h4>
 		<p>Eurofurence offers every registered fursuiter one free personal fursuit badge.
 		If you are bringing more than one fursuit, you can of course get a badge for each of them. For these additional badges we will charge you the small fee of &euro;2 each.
 		Getting a fursuit badge is optional. It is not mandatory to wear it.</p> 
 	</div>
 
-	<div class="retain">
+	<div>
 		<p>You can apply for your fursuit badges only after you have registered as an attendee. To have your suit badge ready for pickup during your check-in, log in with your registration-system login data at:<br/><br/>
 		<a href="https://fursuit.eurofurence.org">https://fursuit.eurofurence.org/</a></p>
 		<p>You can find your login data in your Eurofurence registration confirmation mail.</p>
 	</div>
 
-	<div class="retain">
+	<div>
 		<h4>Fursuit Lounge</h4>
 		<p>All fursuiters have access to the Fursuit Lounge. This is a fursuiters-only area where you can change into your costume, go headless, relax and have access to cold water and some sweet and salty snacks to keep your blood sugar and mineral supply at a healthy level.</p>
 		<p>The Fursuit Lounge is also home of the pipe monster, aka the fursuit drying rack. Aside several normal racks to hang your fursuit, we provide about 60 spaces connected to a turbo fan powered drying system, to get your suit, head and paws dry again in no time.</p>
 		<p>However, with about 1000 suiters around, we highly encourage to check on your drying costume regularly and remove it from the dryer as soon as possible, to make room for someone elses costume. Thank you in advance.</p>
-		<p>To respect the privacy and comfort of attending fursuiters, access is limited to costumers only. Photography and videotaping are prohibited in the lounge.	The lounge is open from the convention opening ceremonies to Sunday noon. It will be locked during the night.</p>
+		<p>To respect the privacy and comfort of attending fursuiters, access is limited to costumers only. Photography and videotaping are prohibited in the lounge.	The lounge is open during the following times and will be locked during the night:</p>
+		<p>SUN&ndash;WED 09:00&ndash;04:00<br />or 30 minutes after end of last event<br />(whichever comes last!)</p>
 	</div>
 
-	<div class="retain">
+	<div>
 		<h4>Fursuiters with no room at the Hotel</h4>
 		<p>Our &#8216;no-room&#8217;-guests are welcome to leave their costumes in the lounge, if they wish to.
 		If you do so, it is at your own risk. Eurofurence e.V. is not liable for anything left in the lounge.
 		In the past, we noticed that a lot of no-room suiters left their suits on the dryer from the evening after their last use, until next day (after)noon, when they came in after panels or alike. DO NOT DO THIS. Please bring your suit to the security depot or at least remove all your parts from the dryer before you leave for the night, and put them on a normal rack.</p>
 	 </div>
 
-	<div class="retain">
+	<div>
 		<h4>Fursuit Photoshoot &amp; Group Photo</h4>
 		<p>The details for these events are not yet final. We will update this as soon as the info is available.</p>
 	</div>
 		
-	<div class="retain">
+	<div>
 		<h4>Fursuit Panel Support</h4>
 		<p>If you are hosting a public fursuit event, a show or panel, we can also support you with a few fans and water dispensers. Just inform the programming guys about this when submitting your panel, and check back with us, once your spot is confirmed. (And no, sorry, we do not offer this service for private room parties, as even our resources are limited. ;-) )</p>
 	</div>
 
-	<div class="retain">
+	<div>
 		<h4>Anything else</h4>
 		<p>If you have any further questions, do not hesitate to contact <a href="https://help.eurofurence.org/contact/fursuit-support" target="_blank">the Fursuit Team</a>.
 	</div>
