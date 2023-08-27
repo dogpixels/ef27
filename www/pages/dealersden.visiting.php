c526b74b93696d2f2256f6c61a3a7dd79cdef7ef
draconigen <draconigen@gmail.com>
2023-08-27 18:37:09 +0200

    feat: :sparkles: add Dealers' Den Visiting page

---
+++ www/pages/dealersden.visiting.php
@@ -0,0 +1,78 @@
+<h1>Visiting the Dealers’ Den</h1>
+
+<section class="uk-column-1-2@l">
+	<p>Welcome to the Eurofurence Dealers' Den! This is your ultimate guide to dive into this creative realm, where imagination knows no bounds. Here, talented artists, creators, and vendors come together to create a vibrant experience of creativity and passion. Whether you're looking for custom art, merchandise, or something else entirely, you'll find it here!</p>
+
+	<div>
+		<h3>What is the Dealers’ Den?</h3>
+		<p>The Dealers' Den is the convention’s marketplace, that brings together over 150 of the furry fandom’s most talented artists and creators from Europe and beyond! This is the place where you can explore a diverse range of offerings, from furry merchandise and exclusive prints to custom commissions and masterfully crafted fursuits and much more. Whether you're an experienced Eurofurence Attendee or a curious adventurer stepping in for the first time, treasures await at every corner.</p>
+		<p>But that's not all! The Dealers’ Den is also the home of the Eurofurence Con Store, where you can grab the official convention T-shirt and other lovely souvenirs from this year and years past. It's your one-stop shop for all things Eurofurence, combining the talents and creativity of the community with the spirit of the convention.</p>
+	</div>
+
+	<div class="uk-margin-bottom">
+		<h3>Official Map</h3>
+		<p>Consult our official map to better navigate the sprawling landscape of the Dealers' Den. The map provides a detailed layout of the space, identifying the location of each artist, creator, and vendor. With this resource in hand, you can efficiently plan your journey through the Dealers’ Den, making sure you visit all the tables that capture your interest. </p>
+
+		<div uk-lightbox>
+			<a href="img/pages/dealersden-visiting/2023-08_Dealers-Den-Map.jpg">
+				<img src="img/pages/dealersden-visiting/2023-08_Dealers-Den-Map.thumb.jpg" alt="Dealers Den Map thumbnail">
+			</a>
+		</div>
+	</div>
+
+	<div>
+		<h3>Opening Times</h3>
+		<p>The Dealers' Den operates on a specific schedule designed to maximize your time for discovery and engagement with our talented community of artists and creators. The timing for each day is as follows:</p>
+		<ul>
+			<li><strong>Monday, September 4th:</strong> 14:00 - 18:00 (Super Sponsor early access at 12:00; Sponsor early access at 13:00)</li>
+			<li><strong>Tuesday, September 5th:</strong> 14:00 - 18:00 (Super Sponsor / Sponsor early access at 13:00)</li>
+			<li><strong>Wednesday, September 6th:</strong> 12:00 - 18:00 </li>
+		</ul>
+	</div>
+
+	<div>
+		<h3>Additional Information</h3>
+		<ul>
+			<li><strong>Payment Methods:</strong> The majority of dealers are equipped to accept credit card payments. Alternatives like Cash, PayPal and even Stripe can also be available. For a seamless purchase experience, kindly inquire about your preferred payment method before making a transaction.</li>
+			<li><strong>Accessibility:</strong> The Dealers' Den is wheelchair accessible. If an Attendee has a "Handicapped ID" (Behindertenausweis) with the German signs außergewöhnlich gehbehindert ("aG"), blind ("Bl") or hilflos ("H") or they are (without handicapped ID) disabled in regards of walking or standing (i.e. after an operation or because of an injury such as an broken foot/leg) or in need of a wheelchair the Attendee is not required to queue themselves at all for the Dealers’ Den. The Eurofurence Security will let them enter directly into the Dealers’ Den during all opening hours.</li>
+			<li>
+				<strong>Visitor Etiquette and Rules:</strong> Before diving into the Dealers' Den, please take a moment to familiarize yourself with our Art & Crafts Rules of the House & Etiquette guidelines, which are detailed on the two posters displayed below.
+				<div uk-lightbox>
+					<a href="img/pages/dealersden-visiting/Etiquette.jpg">
+						<img src="img/pages/dealersden-visiting/Etiquette.thumb.jpg" alt="Etiquette poster" />
+					</a>
+					<a href="img/pages/dealersden-visiting/Rules_of_the_House.jpg">
+						<img src="img/pages/dealersden-visiting/Rules_of_the_House.thumb.jpg" alt="Rules of the House poster" />
+					</a>
+				</div>
+			</li>
+			<li><strong>Lost & Found:</strong> Misplaced something? Head to the Lost & Found booth located at the Security Desk or approach the team at the Dealers’ Den Help desk.</li>
+			<li><strong>NeurodiversityAdvisory:</strong>  The Dealers' Den is a lively and bustling area, which can sometimes result in crowded conditions, elevated noise levels, and various scents. This environment may not be comfortable for everyone, particularly for neurodiverse individuals who may be sensitive to sensory stimuli.
+			If you find the ambiance overwhelming or need assistance, you have multiple options:</li>
+			<ul>
+				<li>You can approach a member of the Dealers' Den Team or Security, who are here to assist you in a discreet and efficient manner.</li>
+				<li>Alternatively, you can head to the Dealers' Den Help Desk for guidance and support.</li>
+			</ul>
+		</ul>
+		<p>For those seeking a calmer shopping experience, we've observed that the Dealers' Den tends to be less crowded during the last hour of its opening times. Consider visiting during this window for a more relaxed atmosphere.</p>
+	</div>
+
+	<div>
+		<h3>FAQ</h3>
+		<li>
+			<strong>Q: Can I commission artwork at the Dealers' Den?</strong><br />
+			A: Yes, many artists take commissions. It's best to discuss details and payment directly with the artist.
+		</li>
+		<li>
+			<strong>Q: Is there a limit to how many items I can purchase?</strong><br />
+			A: Generally, no. But certain exclusive items may have a purchase limit. Check with individual dealers for more information.
+		</li>
+	</div>
+</section>
+
+<hr />
+
+<section>
+	<p>Feel free to browse and most importantly, enjoy your time at the Eurofurence Dealers' Den!</p>
+	<p>Please note: Changes to the layout, dealer listings or opening times may occur. Major updates will be announced via the official <a href="https://app.eurofurence.org/" target="_blank">Eurofurence app</a> and <a href="https://app.eurofurence.org/" target="_blank">Social Media</a>.</p>
+</section>
